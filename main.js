b08ff80d803ff1418b189334c81858f2e0264b72
Ri2-4 <ri.025496@gmail.com>
2025-04-11 02:06:33 +0530

    updated html

--- main.js
+++ main.js
@@ -5,3 +5,12 @@ var typed = new Typed(".text", {
     backDelay: 1000,
     loop: true
 });
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll('.progress-line span').forEach(el => {
+        setTimeout(() => {
+            el.style.transform = "scaleX(1)"; // Animate the progress bar
+        }, 500); // Delay for smooth effect
+    });
+});
+
+
